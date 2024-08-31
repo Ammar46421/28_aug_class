e83622fc94da091ec867058892d2f72c316a3e03
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2024-08-31 22:27:04 +0500

    update

---
+++ app.js
@@ -0,0 +1,11 @@
+var arr =[]
+var obj ={}
+
+
+var firstNm = document.getElementById('firstName')
+var lastNm = document.getElementById('firstName')
+var p_age = document.getElementById('age')
+
+function searchTable(){
+    
+}
